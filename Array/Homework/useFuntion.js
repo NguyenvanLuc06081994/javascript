e09297ec4266dc39671d40149fd7011efd7a4377
NguyenvanLuc06081994 <nvluc0608@gmail.com>
2020-06-02 21:36:57 +0700

    update 28th

---
+++ Array/Homework/useFuntion.js
@@ -0,0 +1,12 @@
+
+
+
+
+// let myColor = ["red", "Green", "White", "Black"];
+// console.log(myColor.toString());
+// console.log(myColor.join('-----'));
+// console.log(myColor.join('+'));
+
+
+
+
--- demo-array.js
+++ demo-array.js
@@ -1,12 +1,17 @@
-let arr = [1,3,4];
-
-function insertFirst(value) {
-    for (let i = arr.length; i > 0; i--) {
-        arr[i] = arr[i-1];
-    }
-    arr[0] = value;
-}
-insertFirst(10);
-arr.splice(2,0,47,80);
-arr.reverse();
-console.log(arr);
+// let arr = [1,3,4];
+//
+// function insertFirst(value) {
+//     for (let i = arr.length; i > 0; i--) {
+//         arr[i] = arr[i-1];
+//     }
+//     arr[0] = value;
+// }
+// insertFirst(10);
+// arr.splice(2,0,47,80);
+// arr.reverse();
+// console.log(arr);
+//
+// let x = [1,3,5,8];
+// x.pop();
+// // x.push(7);
+// console.log(x);
